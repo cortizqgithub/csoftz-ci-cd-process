bace303e36eecd9396b39761201f98d22d77ca16
Carlos Adolfo Ortiz Quiros <carlosaortiz@yahoo.com>
2020-05-23 22:26:02 -0500

    Adds CodeCov data with JaCoCo

---
+++ src/main/java/com/csoftz/cicd/process/service/PeopleService.java
@@ -0,0 +1,7 @@
+package com.csoftz.cicd.process.service;
+
+public class PeopleService {
+    public Integer add(Integer a, Integer b) {
+        return a + b;
+    }
+}
--- src/test/java/com/csoftz/cicd/process/CicdProcessApplicationTests.java
+++ src/test/java/com/csoftz/cicd/process/CicdProcessApplicationTests.java
@@ -1,13 +1,20 @@
 package com.csoftz.cicd.process;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootTest
 class CicdProcessApplicationTests {
+	@Autowired
+	private ApplicationContext appContext;
 
 	@Test
 	void contextLoads() {
+		assertThat(appContext).isNotNull();
 	}
 
 }
